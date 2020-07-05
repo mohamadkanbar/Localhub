49b7d20d47b14ba56eaea879fe5520746376006c
Mohammad KANBAR <mohammadkanbar@MacBook-Pro.local>
2020-07-05 03:52:55 +0200

    acitve

--- admin/ajax.php
+++ admin/ajax.php
@@ -3,7 +3,7 @@
 include_once __DIR__."/../Core/config2.php";
 
 
-
+//activ and unactive user
 extract($_POST);
 $user_id=$conn->real_escape_string($id);
 $status=$conn->real_escape_string($status);
