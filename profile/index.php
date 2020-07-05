910341c1e1ece6cb6ee4c946525dea26e904f9fa
Mohammad KANBAR <mohammadkanbar@MacBook-Pro.local>
2020-07-05 03:51:11 +0200

    like

--- profile/index.php
+++ profile/index.php
@@ -5,6 +5,9 @@ include_once 'inc/topHeader.php' ?>
 include_once "inc/header.php";
 include_once "inc/navbar.php";
 include_once "../Core/config2.php";
+
+$userid = $_SESSION['user']["id"];
+
 ?>
 <div class="row">
     <!-- Import & Export link -->
@@ -64,8 +67,64 @@ include_once "../Core/config2.php";
                     </i>
                 </td>
                 <td>
-              
-                </td>
+  
+                <?php
+                        if (isset($_POST['liked'])) {
+                            $idannouncement = $_POST['idannouncement'];
+                            $result = mysqli_query($conn, "SELECT * FROM Announcement WHERE id=$idannouncement");
+                            $row = mysqli_fetch_array($result);
+                            $n = $row['field4'];
+
+                            mysqli_query($conn, "INSERT INTO liketable (iduser, idannouncement) VALUES ($userid, $idannouncement)");
+                            mysqli_query($conn, "UPDATE Announcement SET field4=$n+1 WHERE id=$idannouncement");
+
+                            echo $n+1;
+                            exit();
+                        }
+                        if (isset($_POST['unliked'])) {
+                            $idannouncement = $_POST['idannouncement'];
+                            $result = mysqli_query($conn, "SELECT * FROM Announcement WHERE id=$idannouncement");
+                            $row = mysqli_fetch_array($result);
+                            $n = $row['field4'];
+
+                            mysqli_query($conn, "DELETE FROM liketable WHERE idannouncement=$idannouncement AND iduser=$userid");
+                            mysqli_query($conn, "UPDATE Announcement SET field4=$n-1 WHERE id=$idannouncement");
+                            
+                            echo $n-1;
+                            exit();
+                        }
+
+                        // Retrieve Announcement from the database
+                        $Announcement = mysqli_query($conn, "SELECT * FROM Announcement ");
+                ?>
+                <?php while ($row = mysqli_fetch_array($Announcement)) { 
+                    ?>
+
+                <div class="post">
+                    <!-- <?php echo $row['title']; ?> -->
+
+                    <div style="padding: 2px; margin-top: 5px;">
+                    <?php 
+
+                        // determine if user has already liked this post
+                        $results = mysqli_query($conn, "SELECT * FROM liketable WHERE iduser=$userid AND idannouncement=".$row['id']."");
+
+                        if (mysqli_num_rows($results) == 0 ): ?>
+                            <!-- user already liketable post -->
+                            <span class="unlike fa fa-thumbs-up" data-id="<?php echo $row['id']; ?>"></span> 
+                            <span class="like hide fa fa-thumbs-o-up" data-id="<?php echo $row['id']; ?>"></span> 
+                        <?php else: ?>
+                            <!-- user has not yet liked post -->
+                            <span class="like fa fa-thumbs-o-up" data-id="<?php echo $row['id']; ?>"></span> 
+                            <span class="unlike hide fa fa-thumbs-up" data-id="<?php echo $row['id']; ?>"></span> 
+                        <?php endif ?>
+
+                        <span class="likes_count"><?php echo $row['field4']; ?> likes</span>
+                    </div>
+                </div>
+
+                <?php } ?>
+                 </td>
             </tr>
 
         <?php } }else{ ?>
@@ -105,27 +164,53 @@ include_once "../Core/config2.php";
 <!-- end script for select  -->
 
 
+<!-- Add Jquery to page -->
+<script>
+	$(document).ready(function(){
+		// when the user clicks on like
+		$('.like').on('click', function(){
+			var idannouncement = $(this).data('id');
+			    $post = $(this);
+
+			$.ajax({
+				url: 'index.php',
+				type: 'post',
+				data: {
+					'liked': 1,
+					'idannouncement': idannouncement
+				},
+				success: function(response){
+					$post.parent().find('span.likes_count').text(response + " field4");
+					$post.addClass('hide');
+					$post.siblings().removeClass('hide');
+				}
+			});
+		});
+
+		// when the user clicks on unlike
+		$('.unlike').on('click', function(){
+			var idannouncement = $(this).data('id');
+		    $post = $(this);
+
+			$.ajax({
+				url: 'index.php',
+				type: 'post',
+				data: {
+					'unliked': 1,
+					'idannouncement': idannouncement
+				},
+				success: function(response){
+					$post.parent().find('span.likes_count').text(response + " field4");
+					$post.addClass('hide');
+					$post.siblings().removeClass('hide');
+				}
+			});
+		});
+	});
+</script>
 
 <!-- Start script for like -->
-    <!-- <script type="text/javascript">
-    $(document).on('click','.status_checks',function(){
-        var status = ($(this).hasClass("btn-success")) ? '0' : '1';
-        var msg = (status=='0')? 'Unlike the Announcement' : 'Like the Announcement';
-        if(confirm("Are you sure to "+ msg)){
-            var current_element = $(this);
-            url = "ajax.php";
-            $.ajax({
-            type:"POST",
-            url: url,
-            data: {id:$(current_element).attr('data'),status:status},
-            success: function(data)
-            {   
-                location.reload();
-            }
-            });
-        }      
-        });
-    </script> -->
+
 <!-- End script for like -->
 
 
